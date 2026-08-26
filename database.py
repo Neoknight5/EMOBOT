c5079baf9b64f3da11012fb91270a218a73f39ea
Neoknight5 <anuradhajayale8@gmail.com>
2026-08-26 10:40:31 +0530

    Update database.py

--- database.py
+++ database.py
@@ -711,5 +711,7 @@ if data[1] >= 5000000 :
     print("🟩🟩🟩")
 if not coins.user_exists(1486400095317659880):
     coins.add_user(1486400095317659880,55000,0,0,0,"","","")
+
+print("🟦🟦 CODE SAVED 🟦🟦")
     
 
