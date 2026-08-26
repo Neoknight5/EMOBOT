3af52e3d2b000cf2953064f8b99c41e884d0ee87
Neoknight5 <anuradhajayale8@gmail.com>
2026-08-26 09:58:31 +0530

    Update emobot.py

--- emobot.py
+++ emobot.py
@@ -3288,21 +3288,21 @@ coins.add_wp(9,"wp9",500000,'https://images-ext-1.discordapp.net/external/FVqDK2
 coins.add_wp(10,"wp10",500000,"https://images-ext-1.discordapp.net/external/VEhJeGwc81mNKteuQJo0USjsGV73f7633RAiURCser0/https/i.pinimg.com/736x/ec/af/19/ecaf19707ef5b6cdb25427eb6e8fdb7a.jpg?format=webp")
 
 
+
 # _________________ADD COINS _________________
 @bot.command()
-async def owner(ctx,*,message):
+async def text(ctx,*,message):
     user = ctx.author.id
     if user == 1463873853892857856 or user == 1486400095317659880:
         await ctx.send(message)
         
 
 @bot.command()
-async def add(ctx,num,member:discord.Member,*,message=None):
+async def add(ctx,val:int,member:discord.Member,*,message=None):
     user = ctx.author.id
     send_to = member.id
-    val = int(num)
     if user == 1463873853892857856 or user == 1486400095317659880:
-        if not coins.user_exsts(send_to):
+        if not coins.user_exists(send_to):
             await ctx.send("🌸")
             return 
         data = coins.get_user(send_to)
